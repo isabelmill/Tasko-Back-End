fa8cb0cbdd477d296a2c8eb1c000f14dc83ac55e
EL00SE <elooseghantous@gmail.com>
2025-08-16 18:52:33 +0300

    connecting to frontend

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000','https://tasko-front-end.onrender.com'],
         credentials: true
     }
     app.use(cors(corsOptions))
