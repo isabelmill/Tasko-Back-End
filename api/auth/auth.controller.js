f1623aa9dbd0684905a99f7aa96ae35243f79baf
EL00SE <elooseghantous@gmail.com>
2025-08-17 09:34:54 +0300

    controller fix

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -2,23 +2,18 @@ const authService = require('./auth.service')
 const logger = require('../../services/logger.service')
 
 async function login(req, res) {
-    const {
-        username,
-        fullname,
-        password
-    } = req.body
+    const { username, password } = req.body   // only username & password
     try {
-        const user = await authService.login(username, password,fullname)
+        const user = await authService.login(username, password)
+        if (!user) return res.status(401).send({ err: 'Invalid credentials' })
+
         req.session.user = user
         res.json(user)
     } catch (err) {
         logger.error('Failed to Login ' + err)
-        res.status(401).send({
-            err: 'Failed to Login'
-        })
+        res.status(500).send({ err: 'Failed to Login' })
     }
 }
-
 async function signup(req, res) {
     try {
         const {
